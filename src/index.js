e862bd3d7fed6168c8444d974454257c45ba8ad8
rebecca-keen <rebeccakeen@gmail.com>
2026-06-02 19:20:35 -0400

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { hydrate, render } from 'react-dom';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<React.StrictMode><App /></React.StrictMode>);
+const rootElement = document.getElementById('root');
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
